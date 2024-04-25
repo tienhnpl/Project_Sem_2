a9df69f766eefcda96f71e2ca2073fd5d08ce574
tienhnpl <dotien.21797@gmail.com>
2024-04-25 19:59:36 +0700

    project

---
+++ project2/src/MasterLayout/MasterLayout.js
@@ -0,0 +1,14 @@
+import {memo} from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+
+function MasterLayout({children,...props}){
+    return(
+        <div {...props}>
+        <Header/>
+        {children}
+        <Footer/>
+        </div>
+    );
+}
+export default memo(MasterLayout);
---
+++ project2/src/Pages/users/Home.js
@@ -0,0 +1,10 @@
+import {memo} from "react";
+
+function Home(){
+    return(
+        <>
+        <h1>Home</h1>
+        </>
+    );
+}
+export default memo(Home);
